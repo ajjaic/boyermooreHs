8c24e3b7690450a8ebff8032e5e9f01cd04a3139
Aj <aj@aj.com>
2014-04-15 07:06:39 +0000

    boyer moore substring search. using state monad for bad match table and arrays for representing text and pattern

---
+++ bmoore.hs
@@ -0,0 +1,60 @@
+import Control.Monad.Trans.State
+import qualified Data.Map.Lazy as Map
+import Data.Array
+
+type Text = String
+type Pattern = String
+type ArrText = Array Int Char
+type ArrPat = Array Int Char
+type Bmtable = Map.Map Char Int
+
+--sample
+text_sample = "trusthardtooxhbrtooth"
+pattern_sample = "tool"
+--
+
+genBadMatchTable :: Pattern -> Bmtable
+genBadMatchTable p = execState (st p $ reverse [1 .. ((plen)-1)]) Map.empty where
+    plen = length p
+    st [x] _         = do l <- get
+                          if Map.member x l
+                            then put l
+                            else put (Map.insert x plen l)
+    st (x:xs) (y:ys) = do l <- get
+                          put (Map.insert x y l)
+                          st xs ys
+
+
+data Substr = Substr { arrt :: ArrText, arrp :: ArrPat} deriving (Show)
+
+makeSubstr :: Text -> Pattern -> Substr
+makeSubstr t p = Substr at ap
+  where
+    at = listArray (1, length t) t
+    ap = listArray (1, length p) p
+
+
+match :: Substr -> Bmtable -> Bool
+match txtpat bmt = comp $ snd bp
+  where
+    (bt, bp) = (bounds t, bounds p)
+    (t, p) = (arrt txtpat, arrp txtpat)
+    comp i = case compfromlast t p i bmt of
+                Nothing -> True
+                Just val -> if val > (snd $ bt)
+                                then False
+                                else comp val
+
+compfromlast :: ArrText -> ArrPat -> Int -> Bmtable -> Maybe Int
+compfromlast t p index bmt = if allequal then Nothing else Just newindex
+  where
+    pindice = snd $ bounds p
+    indices = zip [index, (index-1) ..] [(pindice), (pindice-1) .. 1]
+    allequal = all helper indices
+    helper (ti, pi) = (t ! ti) == (p ! pi)
+    lastcharintext = t ! index
+    newindex = case Map.lookup lastcharintext bmt of
+                    Just v -> v + index
+                    Nothing -> index + pindice
+
+demo = match (makeSubstr text_sample pattern_sample) (genBadMatchTable pattern_sample)
